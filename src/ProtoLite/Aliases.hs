8ea9749e67172af4f8d1d74c1755c158cd56c839
Qiu <lopi258@outlook.com>
2023-08-10 14:57:37 +0800

    * feat: type aliases for reducing parentheses.

---
+++ src/ProtoLite/Aliases.hs
@@ -0,0 +1,20 @@
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+module ProtoLite.Aliases where
+import ProtoLite.Generic
+import Data.Int
+import Data.Word
+
+
+type VInt32 = Variant Int32
+type VInt64 = Variant Int64
+type VUInt32 = Variant Word32
+type VUInt64 = Variant Word64
+type VSInt32 = Variant SInt32
+type VSInt64 = Variant SInt64
+type VBool = Variant Bool
+
+type Fixed32 = Word32
+type Fixed64 = Word64
+type SFixed32 = Int32
+type SFixed64 = Int64
+
